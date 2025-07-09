4d952ad1b1dbf4d214eea19e153890fe0df56869
uDayGetIT <114350224+uDayGetIT@users.noreply.github.com>
2025-07-09 10:43:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './styles/app.css';
+import './styles/app.css'; // or comment out if missing
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
