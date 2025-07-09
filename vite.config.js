87f3de418f2796bbea2a400fd78249ddc276fb6d
uDayGetIT <114350224+uDayGetIT@users.noreply.github.com>
2025-07-09 10:47:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures correct asset loading on Vercel preview URLs
+  base: './',
 });
